694a328689ea95a4363d588e36e2327942997228
luisdk2000 <luis.dk2000@aluno.ifsc.edu.br>
2024-08-16 13:13:25 -0300

    ...

--- SPA_CorEAmbiente/src/routes/siteRoutes.js
+++ SPA_CorEAmbiente/src/routes/siteRoutes.js
@@ -5,14 +5,10 @@ const router = express.Router();
 const siteController = require('../controllers/siteController');
 const sitePublicacaoController = require('../controllers/sitePublicacaoController');
 
-<<<<<<< HEAD
 router.get('/lista-publicacoes', siteController.getPublicacoesPage);
 router.get('/lista-produtos', siteController.getProdutosPage);
 router.get('/lista-sobre_nos', siteController.getSobreNosPage);
 router.get('/lista-faqs', siteController.getFaqsPage);
-=======
-//router.get('/lista-faqs', siteController.getFaqsPage);
->>>>>>> 88bdc3dfbf524ac657372571dad503fac4a1ba87
 router.get('/lista-trabalhos', siteController.getTrabalhosPage);
 router.get('/lista-publicacoes', siteController.getPublicacoesPage);
 router.get('/lista-produtos', siteController.getProdutosPage);
